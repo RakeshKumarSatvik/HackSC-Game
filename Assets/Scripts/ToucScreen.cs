640b3119c1a43dedeffe5f98d3e943596b5ff018
Rakesh Kumar Satvik <satvik@usc.edu>
2015-11-15 04:48:33 -0800

    Justice Car. Initial Version.

---
+++ Assets/Scripts/ToucScreen.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToucScreen : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		Touch myTouch = Input.GetTouch(0);
+	}
+}
